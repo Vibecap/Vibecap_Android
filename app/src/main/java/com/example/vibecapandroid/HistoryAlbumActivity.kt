51359e1ad94e1af15ace52acc522c0419fb4d364
hyeonji0730 <leehyeonji0730@naver.com>
2023-01-06 18:38:09 +0900

    ss

---
+++ app/src/main/java/com/example/vibecapandroid/HistoryAlbumActivity.kt
@@ -0,0 +1,43 @@
+package com.example.vibecapandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class HistoryAlbumActivity : AppCompatActivity() {
+
+    private var recyclerView: RecyclerView? = null
+    private var gridLayoutManager: GridLayoutManager? = null
+    private var arrayList:ArrayList<HistoryAlbumimageClass> ? = null
+    private var historyAlbumAdapters:HistoryAlbumadaptersClass ? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_album)
+
+        recyclerView = findViewById(R.id.recyclerview)
+        gridLayoutManager = GridLayoutManager(applicationContext,3,
+            LinearLayoutManager.VERTICAL,false)
+        recyclerView?.layoutManager = gridLayoutManager
+        recyclerView?.setHasFixedSize(true)
+        arrayList = ArrayList()
+        arrayList = setDataInList()
+        historyAlbumAdapters = HistoryAlbumadaptersClass(applicationContext,arrayList!!)
+        recyclerView?.adapter = historyAlbumAdapters
+    }
+
+    private fun setDataInList(): ArrayList<HistoryAlbumimageClass>{
+        var items: ArrayList<HistoryAlbumimageClass> = ArrayList()
+
+        items.add(HistoryAlbumimageClass(R.drawable.ic_launcher_background))
+        items.add(HistoryAlbumimageClass(R.drawable.ic_launcher_background))
+        items.add(HistoryAlbumimageClass(R.drawable.ic_launcher_background))
+
+
+
+
+        return items
+    }
+}
---
+++ app/src/main/java/com/example/vibecapandroid/HistoryAlbumadaptersClass.kt
@@ -0,0 +1,39 @@
+package com.example.vibecapandroid
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class HistoryAlbumadaptersClass(var context: Context, var arrayList: ArrayList<HistoryAlbumimageClass>):
+    RecyclerView.Adapter<HistoryAlbumadaptersClass.ItemHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        val itemHolder = LayoutInflater.from(parent.context).inflate(R.layout.activity_history_albumgrid,parent,false)
+        return ItemHolder(itemHolder)
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        var historyAlbumImage:HistoryAlbumimageClass = arrayList.get(position)
+        holder.images.setImageResource(historyAlbumImage.image!!)
+
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+
+    class ItemHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var images = itemView.findViewById<ImageView>(R.id.image)
+    }
+}
+
+class HistoryAlbumimageClass {
+
+    var image :Int ? = 0
+
+    constructor(image: Int?) {
+        this.image = image
+    }
+}
