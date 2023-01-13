2fb45f948dbc2d5ae99a32955602648d1af7faf9
ddddabi <ii887742@gmail.com>
2023-01-13 21:46:53 +0900

    vibe 레이아웃 1차 완료

--- app/src/main/java/com/example/vibecapandroid/VibeMainFragment.kt
+++ app/src/main/java/com/example/vibecapandroid/VibeMainFragment.kt
@@ -2,6 +2,7 @@ package com.example.vibecapandroid
 
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
