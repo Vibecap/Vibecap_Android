337e8330d9f28f66ea7c384a44c8c9d5877e1a12
suhoon <jobak4200nim@nate.com>
2023-02-01 23:55:49 +0900

    post 완료

--- app/src/main/java/com/example/vibecapandroid/coms/HomeResponse.kt
+++ app/src/main/java/com/example/vibecapandroid/coms/HomeResponse.kt
@@ -15,29 +15,6 @@ data class Result(
     val vibe_id : Long
 )
 
-data class WEATHER (
-    val response : RESPONSE
-)
-data class RESPONSE (
-    val header : HEADER,
-    val body : BODY
-)
-data class HEADER(
-    val resultCode : Int,
-    val resultMsg : String
-)
-data class BODY(
-    val dataType : String,
-    val items : ITEMS
-)
-data class ITEMS(
-    val item : List<ITEM>
-)
-data class ITEM(
-    val baseData : Int,
-    val baseTime : Int,
-    val category : String
-)
 
 data class DeleteResponse(
     val is_success : Boolean,
