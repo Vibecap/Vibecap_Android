dc3be4d45ae1a32508c5bfd78b04bfcf473b2ee1
ddddabi <ii887742@gmail.com>
2023-02-01 19:59:50 +0900

    230201 API 연결

---
+++ app/src/main/java/com/example/vibecapandroid/MYGlideApp.kt
@@ -0,0 +1,10 @@
+package com.example.vibecapandroid
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+@GlideModule
+class MyGlideApp : AppGlideModule(){
+
+}
--- app/src/main/java/com/example/vibecapandroid/coms/VibePostApiInterface.kt
+++ app/src/main/java/com/example/vibecapandroid/coms/VibePostApiInterface.kt
@@ -9,14 +9,20 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-// 게시물 전체 조회
-interface VibePostAllInterface{
+// 게시물 전체 조회(태그별)
+interface VibePostTagInterface{
     @GET("app/posts")
     fun postAllCheck(
         @Query("tagName") tagName:String
-    ): Call<PostAllResponse>
+    ): Call<PostTagResponse>
 }
 
+// 게시물 전체 조회(weekly)
+interface VibePostWeeklyInterface{
+    @GET("app/posts/weekly")
+    fun postWeeklyCheck(
+    ): Call<PostWeeklyResponse>
+}
 // 게시물 1개 조회
 interface VibePostApiInterface {
     @GET("app/posts/{postId}")
