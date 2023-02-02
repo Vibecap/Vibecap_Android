a3250add382fdace74052f7f28bbaa0fa6555007
suhoon <jobak4200nim@nate.com>
2023-02-02 20:07:40 +0900

    권한 오류 수정

--- app/src/main/java/com/example/vibecapandroid/HistoryPhoneGalleryActivity.kt
+++ app/src/main/java/com/example/vibecapandroid/HistoryPhoneGalleryActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
@@ -12,93 +13,98 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.io.ByteArrayOutputStream
 
 class HistoryPhoneGalleryActivity : AppCompatActivity() {
 
-    val STORAGE = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    val STORAGE = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
     val STORAGE_CODE = 99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //내장 갤러리 사용
-        requestPermissions(STORAGE,STORAGE_CODE)
-        GetAlbum()
+
+        //저장소 읽기 쓰기 권환 획득 및 내장 갤러리 사용
+        checkPermission(STORAGE, STORAGE_CODE)
+
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             STORAGE_CODE -> {
-                for (grant in grantResults) {
-                    if (grant != PackageManager.PERMISSION_GRANTED) {
-                        Toast.makeText(this, "저장소 권한을 승인해 주세요", Toast.LENGTH_LONG).show()
-                    }
-                    else{
-                        val itt = Intent(Intent.ACTION_PICK)
-                        itt.type = MediaStore.Images.Media.CONTENT_TYPE
-                        startActivityForResult(itt, STORAGE_CODE)
-                    }
+                    if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "저장소 권한 승인완료", Toast.LENGTH_LONG).show()
+                        GetAlbum()
                 }
             }
         }
     }
-
 
     fun checkPermission(permissions: Array<out String>, type: Int): Boolean {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             for (permission in permissions) {
                 if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(this, permissions, type)
-                        return false
-                    }
+                    ActivityCompat.requestPermissions(this, permissions, type)
+                    Log.d("권한","권한없음")
+                    return false
+                }
+                else{
+                    GetAlbum()
+                    return true
                 }
             }
-            return true
         }
-
+        GetAlbum()
+        return true
+    }
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-
         if (resultCode == Activity.RESULT_OK) {
             when (requestCode) {
-                    STORAGE_CODE -> {
-                        //uri 얻고 bitmap 으로 변경
-                        val uri = data?.data
-                        val imagebitmap = MediaStore.Images.Media.getBitmap(
-                            applicationContext.getContentResolver(),
-                            uri
-                        )
-                        //감정 없음
-                        feeling = " "
+                STORAGE_CODE -> {
+                    //uri 얻고 bitmap 으로 변경
+                    val uri = data?.data
+                    val imagebitmap = MediaStore.Images.Media.getBitmap(
+                        applicationContext.getContentResolver(),
+                        uri
+                    )
+//                    val stream = ByteArrayOutputStream()
+//                    imagebitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+//                    val byteArray = stream.toByteArray()
+                    //감정 없음
+                    feeling = " "
 
-                        Log.d("imagebitmap","imagebitmap 안나옴")
+                    Log.d("imagebitmap", "imagebitmap")
 
-                        //intent로 bitmap 넘겨주고 다음 activity 실행
-                        val nextIntent = Intent(this, HomeCapturedActivity::class.java)
-                        nextIntent.putExtra("imagebitmap",imagebitmap)
-                        startActivity(nextIntent)
-                    }
+                    //intent로 bitmap 넘겨주고 다음 activity 실행
+                    val nextIntent = Intent(this, HomeCapturedActivity::class.java)
+                    nextIntent.putExtra("imagebitmap", imagebitmap)
+                    startActivity(nextIntent)
+                    finish()
                 }
-            } else {
-                Log.d("촬영취소", "촬영취소")
-                finish()
             }
-
-
+        } else {
+            Log.d("촬영취소", "촬영취소")
+            finish()
         }
-        // 갤러리 취득
-        fun GetAlbum() {
-            if (checkPermission(STORAGE, STORAGE_CODE)) {
-                val itt = Intent(Intent.ACTION_PICK)
-                itt.type = MediaStore.Images.Media.CONTENT_TYPE
-                startActivityForResult(itt, STORAGE_CODE)
-            }
+    }
 
-        }
+    // 갤러리 취득
+    fun GetAlbum() {
+            Log.d("getalbum","getalbum")
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent, STORAGE_CODE)
 
+    }
 }
-
 
--- app/src/main/java/com/example/vibecapandroid/HomeCameraActivity.kt
+++ app/src/main/java/com/example/vibecapandroid/HomeCameraActivity.kt
@@ -28,16 +28,16 @@ class HomeCameraActivity: AppCompatActivity() {
 
     // storage 권한 처리에 필요한 변수
     val CAMERA = arrayOf(Manifest.permission.CAMERA)
-
     val CAMERA_CODE = 98
-    val STORAGE_CODE = 99
-    var checking_camera: Boolean = false
+   
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        requestingPermission(CAMERA,CAMERA_CODE)
-        CallCamera()
+        //카메라 권한 획득 및 내장 카메라로 이동
+        checkPermission(CAMERA,CAMERA_CODE)
+
     }
 
 
@@ -51,14 +51,9 @@ class HomeCameraActivity: AppCompatActivity() {
 
         when (requestCode) {
             CAMERA_CODE -> {
-                for (grant in grantResults) {
-                    if (grant != PackageManager.PERMISSION_GRANTED) {
-                        Toast.makeText(this, "카메라 권한을 승인해 주세요", Toast.LENGTH_LONG).show()
-                    }
-                    else{
-                        val itt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                        startActivityForResult(itt, CAMERA_CODE)
-                    }
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "카메라 권한 승인완료", Toast.LENGTH_LONG).show()
+                    CallCamera()
                 }
             }
 
@@ -66,34 +61,31 @@ class HomeCameraActivity: AppCompatActivity() {
     }
 
     // 다른 권한등도 확인이 가능하도록
-    fun requestingPermission(permissions: Array<out String>, type: Int): Boolean {
+    fun checkPermission(permissions: Array<out String>, type: Int): Boolean {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             for (permission in permissions) {
                 if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(this, permissions, type)
                     return false
                 }
+                else{
+                    CallCamera()
+                    return true
+
+                }
 
             }
         }
+        CallCamera()
         return true
     }
 
     // 카메라 촬영 - 권한 처리
-
      fun CallCamera() {
-           if(requestingPermission(CAMERA, CAMERA_CODE)){
                Log.d("callcamera","callcamera")
                 val itt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
                 startActivityForResult(itt, CAMERA_CODE)
-           }
-
-        }
-
-
-
-
-
+     }
 
     // 결과
         override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
