92af8b861237bf1f984e43d635e3765395ac648a
Jieun Choi <cje172@naver.com>
2023-02-08 23:12:06 +0900

    fix(vibe): 게시물 전체 조회 (tag X) / 검색 / 태그별 더보기 무한 스크롤 구현

--- app/src/main/java/com/example/vibecapandroid/VibeDetailActivity.kt
+++ app/src/main/java/com/example/vibecapandroid/VibeDetailActivity.kt
@@ -3,44 +3,207 @@ package com.example.vibecapandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.widget.Toolbar
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.vibecapandroid.coms.PostContentData
+import com.example.vibecapandroid.coms.PostTagResponse
+import com.example.vibecapandroid.coms.VibePostApiInterface
 import com.example.vibecapandroid.databinding.ActivityVibeDetailBinding
+import com.example.vibecapandroid.utils.getRetrofit
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class VibeDetailActivity : AppCompatActivity() {
-    val viewBinding : ActivityVibeDetailBinding by lazy {
-        ActivityVibeDetailBinding.inflate(layoutInflater)
-    }
+
+    lateinit var binding: ActivityVibeDetailBinding
+
+    private lateinit var mMapLayoutManager: StaggeredGridLayoutManager
+    private lateinit var mListAdapter: VibeMainAllPostsRVAdapter
+    private lateinit var mRecyclerView: RecyclerView
+
+    private var totalCount = 0 // 전체 아이템 개수
+    private var isNext = false // 다음 페이지 유무
+    private var page = -1      // 현재 페이지
+    private var limit = 8     // 한 번에 가져올 아이템 수
+
+    private lateinit var addViewText: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_vibe_detail)
+        binding = ActivityVibeDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolBar_top)
-        setSupportActionBar(toolbar)
-        val ab = supportActionBar!!
-        ab.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+        overridePendingTransition(R.anim.slide_up, R.anim.fade_out)
+
+        // 전달받은 해시태그 이름 반영
+        val intent = intent
+        addViewText = intent.getStringExtra("add_view_text").toString()
+        binding.vibeDetailTitleTv.text = addViewText
+
+        binding.vibeDetailBackBtn.setOnClickListener { finish() }
+
+        // 게시물 RecyclerView adapter 연결
+        mListAdapter = VibeMainAllPostsRVAdapter(this)
+        mMapLayoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        binding.vibeDetailRv.adapter = mListAdapter
+
+        mListAdapter.setMyItemClickListener(object :
+            VibeMainAllPostsRVAdapter.MyItemClickListener {
+            override fun onItemClick(postContentData: PostContentData) {
+                val intent = Intent(this@VibeDetailActivity, VibePostActivity::class.java)
+                intent.putExtra("post_id", postContentData.post_id)
+                startActivity(intent)
+            }
+        })
+
+        val tagName: String = addViewText.substring(1, addViewText.length)
+        getPostsWithTag(tagName)
+        initScrollListener()
 
         // 게시물 작성 열기
-        viewBinding.btnAddpostdetail.setOnClickListener {
-            val intent = Intent(this, HistoryPostActivity::class.java)
-            startActivity(intent)
-        }
+//        viewBinding.btnAddpostdetail.setOnClickListener {
+//            val intent = Intent(this, HistoryPostActivity::class.java)
+//            startActivity(intent)
+//        }
+
     }
 
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.fade_in, R.anim.slide_down)
+    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        when (id) {
-            android.R.id.home -> {
-                finish()
-                return true
+    /**
+     * 게시물 전체 조회 (태그별)
+     */
+    private fun getPostsWithTag(tagName: String) {
+        val vibePostService = getRetrofit().create(VibePostApiInterface::class.java)
+        vibePostService.postAllCheckWithTag(userToken, tagName, getPage())
+            .enqueue(object : Callback<PostTagResponse> {
+                override fun onResponse(
+                    call: Call<PostTagResponse>,
+                    response: Response<PostTagResponse>
+                ) {
+                    Log.d("[VIBE] SEARCH_POSTS/SUCCESS", response.toString())
+                    val resp: PostTagResponse = response.body()!!
+
+                    // 서버 response 중 code 값에 따른 결과
+                    when (resp.code) {
+                        1000 -> {
+                            binding.vibeDetailEmptyTv.visibility = View.GONE
+                            binding.vibeDetailRv.visibility = View.VISIBLE
+
+                            totalCount = resp.result.totalElements
+                            isNext = !(resp.result.last)
+                            mListAdapter.setPosts(resp.result.content as ArrayList<PostContentData>)
+                            binding.vibeDetailRv.scheduleLayoutAnimation()
+                        }
+                        3011 -> {
+                            binding.vibeDetailEmptyTv.visibility = View.VISIBLE
+                            binding.vibeDetailRv.visibility = View.GONE
+                        }
+                        else -> Log.d(
+                            "[VIBE] SEARCH_POSTS/FAILURE",
+                            "${resp.code} / ${resp.message}"
+                        )
+                    }
+                }
+
+                override fun onFailure(call: Call<PostTagResponse>, t: Throwable) {
+                    Log.d("[VIBE] SEARCH_POSTS/FAILURE", t.message.toString())
+                }
+            })
+    }
+
+    // RecyclerView에 더 보여줄 데이터를 로드하는 경우
+    private fun loadMorePosts() {
+        mListAdapter.setLoadingView(true)
+
+        // 너무 빨리 데이터가 로드되면 스크롤 되는 Ui 를 확인하기 어려우므로,
+        // Handler 를 사용하여 1초간 postDelayed 시킴
+        val handler = android.os.Handler()
+        handler.post {
+            binding.vibeDetailRv.post {
+                binding.vibeDetailRv.smoothScrollToPosition(
+                    mListAdapter.itemCount - 1
+                )
             }
-            else -> {}
         }
+        handler.postDelayed({
+            val vibePostService = getRetrofit().create(VibePostApiInterface::class.java)
+            vibePostService.postAllCheck(userToken, getPage())
+                .enqueue(object : Callback<PostTagResponse> {
+                    override fun onResponse(
+                        call: Call<PostTagResponse>,
+                        response: Response<PostTagResponse>
+                    ) {
+                        val resp: PostTagResponse = response.body()!!
 
-        return super.onOptionsItemSelected(item)
+                        // 서버 response 중 code 값에 따른 결과
+                        when (resp.code) {
+                            1000 -> {
+                                totalCount = resp.result.totalElements
+                                isNext = !(resp.result.last)
+                                mListAdapter.run {
+                                    setLoadingView(false)
+                                    addPosts(resp.result.content as ArrayList<PostContentData>)
+                                }
+                            }
+                            else -> Log.d(
+                                "[VIBE] GET_ALL_POSTS/FAILURE",
+                                "${resp.code} / ${resp.message}"
+                            )
+                        }
+                    }
+
+                    override fun onFailure(call: Call<PostTagResponse>, t: Throwable) {
+                        Log.d("[VIBE] GET_ALL_POSTS/FAILURE", t.message.toString())
+                    }
+                })
+        }, 1000)
     }
+
+    private fun getPage(): Int {
+        page++
+        return page
+    }
+
+    private fun hasNextPage(): Boolean {
+        return isNext
+    }
+
+    private fun setHasNextPage(b: Boolean) {
+        isNext = b
+    }
+
+    // scroll이 끝에 닿으면 데이터에 null 추가 및 Adapter에 알림
+    private fun initScrollListener() {
+        binding.vibeDetailRv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                if (!recyclerView.canScrollVertically(1)) {
+                    Log.d("[VIBE-MAIN] Scroll", "최하단")
+
+                    val layoutManager = binding.vibeDetailRv.layoutManager
+                    if (hasNextPage()) {
+                        val lastVisibleItem = (layoutManager as StaggeredGridLayoutManager)
+                            .findLastCompletelyVisibleItemPositions(null)[0]
+
+                        // 마지막으로 보여진 아이템 position 이
+                        // 전체 아이템 개수보다 8개 모자란 경우, 데이터를 loadMore 한다
+                        if (layoutManager.itemCount <= lastVisibleItem + 8) {
+                            loadMorePosts()
+                            setHasNextPage(false)
+                        }
+                    }
+                }
+            }
+        })
+    }
+
 }
--- app/src/main/java/com/example/vibecapandroid/VibeSearchActivity.kt
+++ app/src/main/java/com/example/vibecapandroid/VibeSearchActivity.kt
@@ -1,142 +1,221 @@
 package com.example.vibecapandroid
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
-import android.view.MenuItem
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
-import com.bumptech.glide.Glide
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView.OnEditorActionListener
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.vibecapandroid.coms.PostContentData
 import com.example.vibecapandroid.coms.PostTagResponse
 import com.example.vibecapandroid.coms.VibePostApiInterface
-import com.example.vibecapandroid.coms.VibePostTagInterface
-import com.example.vibecapandroid.databinding.ActivitySearchBinding
+import com.example.vibecapandroid.databinding.ActivityVibeSearchBinding
+import com.example.vibecapandroid.utils.getRetrofit
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 
 class VibeSearchActivity : AppCompatActivity() {
-    val viewBinding : ActivitySearchBinding by lazy {
-        ActivitySearchBinding.inflate(layoutInflater)
-    }
-    @SuppressLint("MissingInflatedId")
+
+    lateinit var binding: ActivityVibeSearchBinding
+
+    private lateinit var vibeSearchLayoutManager: StaggeredGridLayoutManager
+    private lateinit var vibeSearchRVAdapter: VibeMainAllPostsRVAdapter
+    private lateinit var mRecyclerView: RecyclerView
+
+    private var totalCount = 0 // 전체 아이템 개수
+    private var isNext = false // 다음 페이지 유무
+    private var page = -1      // 현재 페이지
+    private var limit = 8     // 한 번에 가져올 아이템 수
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        binding = ActivityVibeSearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        overridePendingTransition(R.anim.slide_in, R.anim.fade_out)
+
+        binding.vibeSearchBackBtn.setOnClickListener { finish() }
+
+        // 게시물 RecyclerView adapter 연결
+        vibeSearchRVAdapter = VibeMainAllPostsRVAdapter(this)
+        vibeSearchLayoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        binding.vibeSearchRv.adapter = vibeSearchRVAdapter
+
+        vibeSearchRVAdapter.setMyItemClickListener(object :
+            VibeMainAllPostsRVAdapter.MyItemClickListener {
+            override fun onItemClick(postContentData: PostContentData) {
+                val intent = Intent(this@VibeSearchActivity, VibePostActivity::class.java)
+                intent.putExtra("post_id", postContentData.post_id)
+                startActivity(intent)
+            }
+        })
 
 
-        // 툴바
-        val toolbar = findViewById<Toolbar>(R.id.toolBar_top)
-        setSupportActionBar(toolbar)
-        val ab = supportActionBar!!
-        ab.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+        // 검색 (tag별)
+        binding.vibeSearchBtn.setOnClickListener {
+            binding.vibeSearchEt.clearFocus()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(binding.vibeSearchEt.windowToken, 0)
 
-        //searchAPI()
-        findViewById<ImageButton>(R.id.imageButton_tagsearch11).setOnClickListener {
-            Toast.makeText(applicationContext, "search success", Toast.LENGTH_SHORT).show()
-
-            // 검색 API 추가
-            searchAPI()
-            // 검색 결과 출력
+            page = -1
+            searchPosts(binding.vibeSearchEt.text.toString())
+            initScrollListener()
         }
+
+        binding.vibeSearchEt.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                binding.vibeSearchEt.clearFocus()
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(binding.vibeSearchEt.windowToken, 0)
+
+                page = -1
+                searchPosts(binding.vibeSearchEt.text.toString())
+                initScrollListener()
+
+                return@OnEditorActionListener true
+            }
+            false
+        })
 
     }
 
-    // api
-    object searchRetrofitObject {
-        private fun getRetrofit(): Retrofit {
-            return Retrofit.Builder()
-                .baseUrl("http://ec2-175-41-230-93.ap-northeast-1.compute.amazonaws.com:8080/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        }
-
-        fun getApiService(): VibePostTagInterface {
-            return getRetrofit().create(VibePostTagInterface::class.java)
-        }
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.fade_in, R.anim.slide_out)
     }
 
-    private fun searchAPI() {
-        val tagName = findViewById<EditText>(R.id.et_search_tag).toString()
-        tagName == "신나는 "
-        searchRetrofitObject.getApiService().postAllCheck(tagName).enqueue(object :
-            Callback<PostTagResponse> {
-            // api 호출 성공시
-            override fun onResponse(call: Call<PostTagResponse>, response: Response<PostTagResponse>) {
-                if (response.isSuccessful){
-                    val responseData = response.body()
-                    if (responseData != null){
-                        //Toast.makeText(applicationContext, "success", Toast.LENGTH_SHORT).show()
-                        Log.d(
-                            "searchResult",
-                            "searchResult\n"+
-                                    "isSuccess:${responseData?.is_success}\n " +
-                                    "Code: ${responseData?.code} \n" +
-                                    "Message:${responseData?.message} \n"
+    /**
+     * 게시물 전체 조회 (태그별)
+     */
+    private fun searchPosts(tagName: String) {
+        val vibePostService = getRetrofit().create(VibePostApiInterface::class.java)
+        vibePostService.postAllCheckWithTag(userToken, tagName, getPage())
+            .enqueue(object : Callback<PostTagResponse> {
+                override fun onResponse(
+                    call: Call<PostTagResponse>,
+                    response: Response<PostTagResponse>
+                ) {
+                    Log.d("[VIBE] SEARCH_POSTS/SUCCESS", response.toString())
+                    val resp: PostTagResponse = response.body()!!
+
+                    // 서버 response 중 code 값에 따른 결과
+                    when (resp.code) {
+                        1000 -> {
+                            binding.vibeSearchEmptyTv.visibility = View.GONE
+                            binding.vibeSearchRv.visibility = View.VISIBLE
+
+                            totalCount = resp.result.totalElements
+                            isNext = !(resp.result.last)
+                            vibeSearchRVAdapter.setPosts(resp.result.content as ArrayList<PostContentData>)
+                            binding.vibeSearchRv.scheduleLayoutAnimation()
+                        }
+                        3011 -> {
+                            binding.vibeSearchEmptyTv.visibility = View.VISIBLE
+                            binding.vibeSearchRv.visibility = View.GONE
+                        }
+                        else -> Log.d(
+                            "[VIBE] SEARCH_POSTS/FAILURE",
+                            "${resp.code} / ${resp.message}"
                         )
-                        if(responseData.is_success){
-                            //val editor = getSharedPreferences(
-                                //"sharecropped",
-                                //MODE_PRIVATE
-                            //).edit()
-                            //editor.putInt("post_id",responseData.result)
-                            //editor.putInt("member_id",responseData.result[0].content[0].member_id)
-                            //editor.putInt("vibe_id",responseData.result[0].content[0].vibe_id)
-                            //editor.putString("vibe_image",responseData.result[1].vibe_image)
-                            //editor.apply()
-                            //stringToBitmap(editor.putString("vibe_image",responseData.result[1].vibe_image).toString())
-                            val imageView = viewBinding.imageViewMain1
-                            val defaultImage = R.drawable.image_ic_activity_history_album_list1
-                            //val url = editor.putString("vibe_image",responseData.result[0].content[0].vibe_image)
-                            val url = "https://firebasestorage.googleapis.com/v0/b/vibecap-ee692.appspot.com/o/b9bf7d74-88f3-4b06-952b-dc9c59f8090ajpg?alt=media"
-                            Glide.with(this@VibeSearchActivity)
-                                .load(url) // 불러올 이미지 url
-                                .placeholder(defaultImage) // 이미지 로딩 시작하기 전 표시할 이미지
-                                .error(defaultImage) // 로딩 에러 발생 시 표시할 이미지
-                                .fallback(defaultImage) // 로드할 url 이 비어있을(null 등) 경우 표시할 이미지
-                                .into(imageView) // 이미지를 넣을 뷰
-                        }else{
-                            if(responseData.code == 3011){
-                                // xml에 tvView 추가해서 문구 띄우기
-                                Toast.makeText(applicationContext,"해당 태그를 가진 게시물이 없습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
 
+                override fun onFailure(call: Call<PostTagResponse>, t: Throwable) {
+                    Log.d("[VIBE] SEARCH_POSTS/FAILURE", t.message.toString())
+                }
+            })
+    }
+
+    // RecyclerView에 더 보여줄 데이터를 로드하는 경우
+    private fun loadMorePosts() {
+        vibeSearchRVAdapter.setLoadingView(true)
+
+        // 너무 빨리 데이터가 로드되면 스크롤 되는 Ui 를 확인하기 어려우므로,
+        // Handler 를 사용하여 1초간 postDelayed 시킴
+        val handler = android.os.Handler()
+        handler.post {
+            binding.vibeSearchRv.post {
+                binding.vibeSearchRv.smoothScrollToPosition(
+                    vibeSearchRVAdapter.itemCount - 1
+                )
+            }
+        }
+        handler.postDelayed({
+            val vibePostService = getRetrofit().create(VibePostApiInterface::class.java)
+            vibePostService.postAllCheck(userToken, getPage())
+                .enqueue(object : Callback<PostTagResponse> {
+                    override fun onResponse(
+                        call: Call<PostTagResponse>,
+                        response: Response<PostTagResponse>
+                    ) {
+                        val resp: PostTagResponse = response.body()!!
+
+                        // 서버 response 중 code 값에 따른 결과
+                        when (resp.code) {
+                            1000 -> {
+                                totalCount = resp.result.totalElements
+                                isNext = !(resp.result.last)
+                                vibeSearchRVAdapter.run {
+                                    setLoadingView(false)
+                                    addPosts(resp.result.content as ArrayList<PostContentData>)
+                                }
                             }
+                            else -> Log.d(
+                                "[VIBE] SEARCH_POSTS/FAILURE",
+                                "${resp.code} / ${resp.message}"
+                            )
+                        }
+                    }
+
+                    override fun onFailure(call: Call<PostTagResponse>, t: Throwable) {
+                        Log.d("[VIBE] SEARCH_POSTS/FAILURE", t.message.toString())
+                    }
+                })
+        }, 1000)
+    }
+
+    private fun getPage(): Int {
+        page++
+        return page
+    }
+
+    private fun hasNextPage(): Boolean {
+        return isNext
+    }
+
+    private fun setHasNextPage(b: Boolean) {
+        isNext = b
+    }
+
+    // scroll이 끝에 닿으면 데이터에 null 추가 및 Adapter에 알림
+    private fun initScrollListener() {
+        binding.vibeSearchRv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                if (!recyclerView.canScrollVertically(1)) {
+
+                    val layoutManager = binding.vibeSearchRv.layoutManager
+                    if (hasNextPage()) {
+                        val lastVisibleItem = (layoutManager as StaggeredGridLayoutManager)
+                            .findLastCompletelyVisibleItemPositions(null)[0]
+
+                        // 마지막으로 보여진 아이템 position 이
+                        // 전체 아이템 개수보다 8개 모자란 경우, 데이터를 loadMore 한다
+                        if (layoutManager.itemCount <= lastVisibleItem + 8) {
+                            loadMorePosts()
+                            setHasNextPage(false)
                         }
                     }
                 }
             }
-
-            // api 호출 실패시
-            override fun onFailure(call: Call<PostTagResponse>, t: Throwable) {
-                Log.e("retrofit Search onFailure2", "${t.message.toString()}")
-            }
         })
-    }
-
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        when (id) {
-            android.R.id.home -> {
-                finish()
-                return true
-            }
-            else -> {}
-        }
-
-        return super.onOptionsItemSelected(item)
     }
 }
